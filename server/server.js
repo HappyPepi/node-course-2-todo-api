2d65ce6dc4ca05126f289f9a742af19b9e7e7a11
Josef Moser <josef.moser90@gmail.com>
2018-07-16 17:19:20 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ const {
 const {
     User
 } = require('./models/user');
-const {authenticate} = require('./middleware/authenticate');
+const {
+    authenticate
+} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -127,6 +129,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
